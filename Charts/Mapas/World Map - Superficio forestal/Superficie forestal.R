c5759cdb45afcabf2e0b6930e477fda937ba5f64
Herbert Lizama <66571148+herbert-liz@users.noreply.github.com>
2023-10-15 20:32:16 -0600

    Update Superficie forestal.R

--- Charts/Mapas/World Map - Superficio forestal/Superficie forestal.R
+++ Charts/Mapas/World Map - Superficio forestal/Superficie forestal.R
@@ -10,10 +10,10 @@ library(viridis)
 library(maps)
 
 # 1. Consultamos los indicadores sobre algun tema de interes ####
-index_search <- wb_search(pattern = '.')
+index_search <- wb_search(pattern = 'tree')
 
 # 2. Seleccionamos indicador, en este caso ####
-datos_indicador <- wb(country = "countries_only",indicator = "EN.URB.LCTY.UR.ZS", 
+datos_indicador <- wb(country = "countries_only",indicator = "AG.LND.FRST.K2", 
                       startdate = 2021, enddate = 2021)
 
 # 3. Descargamos informacion del mapa mundial ####
@@ -25,10 +25,8 @@ map_urban <- left_join(world_map, datos_indicador, by = c("iso_a2" = "iso2c"))
 # 5. Creamos mapa ####
 urban_graph <- ggplot() +
   geom_sf(data = map_urban, aes(fill = value)) +
-  coord_sf(xlim = c(-20, 114), 
-           ylim = c(-37, 37)) +
   scale_fill_gradient(low = "#4ccae0", high = "#113f40") +
-  ggtitle("Porcentaje de poblacion en la ciudad más habitada (2021)") +
+  ggtitle("Superficie forestal en KM cuadtrados | 2020") +
   theme_bw() +
   labs(caption = "Data Source: World Bank") + 
   theme(plot.title = element_text(hjust = 0.5, face = "bold"),
